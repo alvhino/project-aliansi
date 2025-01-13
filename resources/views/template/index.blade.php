912a75565598a1173e39d004863f77634186de95
remasnarmusaba <remasnarmsb@gmail.com>
2025-01-13 08:25:43 +0700

    melakukan perubahan pada folder view

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -5,12 +5,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <title>TVRI</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ url('assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ url('assets/img/2.png') }}" rel="icon">
 <link href="{{ url('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
 <!-- Google Fonts -->
