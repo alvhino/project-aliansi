869fd030e80f10d449a43161259daafbc912146c
remasnarmusaba <remasnarmsb@gmail.com>
2025-01-16 08:10:09 +0700

    melakukan perubahan pada folder riwayat

--- resources/views/list_kerjasama/index.blade.php
+++ resources/views/list_kerjasama/index.blade.php
@@ -26,7 +26,7 @@
         <div class="list-group-item d-flex justify-content-between align-items-center px-4 py-3" 
              style="background-color: #f8f9fa; border: 1px solid #e0e0e0; border-radius: 8px; margin-bottom: 10px;">
             <div>
-                <h5 class="mb-1 fw-bold" style="font-size: 18px;"> <a href="#" style="color: black;"> {{ $item['name'] }} </a></h5>
+                <h5 class="mb-1 fw-bold" style="font-size: 18px;"> <a href="" style="color: black;"> {{ $item['name'] }} </a></h5>
                 <p>{{ $item['detail'] }}</p>
             </div>
             <a href="#" class="text-decoration-none text-dark" style="font-size: 20px; transform: rotate(-45deg);">&#8594;</a>
